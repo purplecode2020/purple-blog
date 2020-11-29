e4988f900bebadd863f106020c119603bed8c3e8
purplecode2020 <purplecode2020@gmail.com>
2020-11-29 10:30:30 +0900

    Initial Setting

---
+++ README.md
@@ -0,0 +1,3 @@
+# PurpleCode Leader Tech Blog 😆
+
+> IT 동아리 PurpleCode 회장의 Tech Blog 
---
+++ gatsby-meta-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  title: `PurpleCode Tech`,
+  description: `IT 동아리 PurpleCode Leader Tech Blog`,
+  author: `purplecode2020`,
+  introduction: `IT 동아리 PurpleCode의 회장직을 맞고 있으며 현재 FullStack Developer로 활동 중인 이재준입니다.`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `purplecode2020`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: `purplecode2020`, // Your Facebook account
+    linkedin: ``,
+    instagram: `purple._.code`,
+    main: `purplecode2020@gmail.com`
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [
+    `blog`,
+    `tech`,
+    `frontend`,
+    `backend`,
+    `javascript`,
+    `typescript`,
+    `react`,
+    `react-native`,
+  ],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'purplecode2020',
+  },
+  share: {
+    facebookAppId: '671218186926369', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: 'UA-167524196-1', // Add your google analytics tranking ID
+}
